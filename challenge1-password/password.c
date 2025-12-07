619a56cc869d6a4a4ab13450e2f74ce16567b1ef
Yacine BEN BELKACEM <yacine.benbelkacem@outlook.com>
2025-12-07 23:33:58 +0100

    Part 1

--- challenge1-password/password.c
+++ challenge1-password/password.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INPUT_FILE_NAME input
+#define INPUT_FILE_NAME "input"
 
 int main(){
+	int pos = 50;
+	int dist, count = 0;
+	char dir;
 
+	FILE* f = fopen(INPUT_FILE_NAME, "rw");
 
+	fseek(f, 0, SEEK_END);
 
+	int size = ftell(f);
+
+	fseek(f, 0, SEEK_SET);
+
+	printf("Total combinations : %d \n", size);
+
+	while(!feof(f)){
+
+		fscanf(f,"%c%d\n",&dir,&dist);
+
+		if ( dir == 'R' ){
+			pos += dist;
+		}else{
+			pos -= dist;
+		}
+
+		pos = pos % 100;
+
+		if ((pos == 0))
+			count ++;
+
+	}
+
+	printf("password --> %d\n",count);
+
+	fclose(f);
+
+	return 0;
 }
